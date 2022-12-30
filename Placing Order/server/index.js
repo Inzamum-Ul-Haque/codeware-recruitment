e771568e7a915cb074e7652eb8028f9f059ef172
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-31 00:47:01 +0600

    final commit

--- Placing Order/server/index.js
+++ Placing Order/server/index.js
@@ -5,13 +5,13 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 // define port and app
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-const uri =
-  "mongodb+srv://inzy_98:agent007@cluster0.afwac63.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afwac63.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
